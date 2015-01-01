1458a86b51f524ed5041877382d57f11abd778cb
Nevo Segal <nevosegal@gmail.com>
2015-01-01 13:01:37 +0200

    init

---
+++ src/ofApp.cpp
@@ -0,0 +1,146 @@
+/* This is an example of how to integrate maximilain into openFrameworks,
+ including using audio received for input and audio requested for output.
+ 
+ 
+ You can copy and paste this and use it as a starting example.
+ 
+ */
+
+
+#include "ofApp.h"
+
+
+
+//-------------------------------------------------------------
+ofApp::~ofApp() {
+    
+    //    delete beat.myData; /*you should probably delete myData for any sample object
+    //that you've created in ofApp.h*/
+    
+}
+
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    /* some standard setup stuff*/
+    
+    ofEnableAlphaBlending();
+    ofSetupScreen();
+    ofBackground(0, 0, 0);
+    ofSetVerticalSync(true);
+    
+    /* This is stuff you always need.*/
+    
+    sampleRate 			= 44100; /* Sampling Rate */
+    initialBufferSize	= 512;	/* Buffer Size. you have to fill this buffer with sound*/
+    
+    
+    /* Now you can put anything you would normally put in maximilian's 'setup' method in here. */
+    
+    
+//    beat.load(ofToDataPath("beat2.wav"));
+//    beat.getLength();
+    
+    
+    ofSoundStreamSetup(2,0,this, sampleRate, initialBufferSize, 4);/* Call this last ! */
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    /* You can use any of the data from audio received and audiorequested to draw stuff here.
+     Importantly, most people just use the input and output arrays defined above.
+     Clever people don't do this. This bit of code shows that by default, each signal is going to flip
+     between -1 and 1. You need to account for this somehow. Get the absolute value for example.
+     */
+    
+    ofSetColor(255, 255, 255,255);
+    ofCircle(200, 300, wave*150);
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::audioRequested 	(float * output, int bufferSize, int nChannels){
+    
+    for (int i = 0; i < bufferSize; i++){
+
+        wave=sine1.sinebuf(440);/* mouse controls sinewave pitch. we get abs value to stop it dropping
+                                         //										 delow zero and killing the soundcard*/
+        
+        mymix.stereo(wave, outputs, 0.5);
+        
+        
+        output[i*nChannels    ] = outputs[0]; /* You may end up with lots of outputs. add them here */
+        output[i*nChannels + 1] = outputs[1];
+    }
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::audioReceived 	(float * input, int bufferSize, int nChannels){
+    
+    
+    /* You can just grab this input and stick it in a double, then use it above to create output*/
+    
+    for (int i = 0; i < bufferSize; i++){
+        
+        /* you can also grab the data out of the arrays*/
+        
+        
+    }
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
